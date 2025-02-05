671f18674acb9519e12b36279e6404c7d4ce5ee9
Piernas Loïc <58866082+lpi-code@users.noreply.github.com>
2025-02-05 22:48:20 +0000

    fix pip requirements

--- setup.sh
+++ setup.sh
@@ -13,6 +13,6 @@ cd lpi-code.metatrader
 
 python3 -m venv .venv
 
-pip install -r dev-requirements.txt
+pip install -r .devcontainer/dev-requirements.txt
 ansible-galaxy install -r requirements.yml
 ansible-galaxy role install lpi-code.metatrader
