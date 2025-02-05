ed970c9f58a54cf7b98afe80d5c6eb00cd86a0a6
Piernas Loïc <58866082+lpi-code@users.noreply.github.com>
2025-02-05 22:47:36 +0000

    clone idempotent

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ sudo apt update
 
 sudo apt install -y ansible python3-venv curl git
 
-git clone https://github.com/lpi-code/lpi-code.metatrader.git 
+if [ ! -d "lpi-code.metatrader" ]; then
+    git clone https://github.com/lpi-code/lpi-code.metatrader.git
+fi
 
 cd lpi-code.metatrader
 
 python3 -m venv .venv
 
-pip insfall -r dev-requirements.txt
+pip install -r dev-requirements.txt
 ansible-galaxy install -r requirements.yml
 ansible-galaxy role install lpi-code.metatrader
